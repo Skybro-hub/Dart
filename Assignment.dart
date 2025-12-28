4a182f362221f6830ca999db256f9d92e09ca59f
Sky_Bro <hafizur15-4179@diu.edu.bd>
2025-12-28 21:46:37 +0600

    Add files via upload

---
+++ Assignment.dart
@@ -0,0 +1,14 @@
+void main() {
+  Set<int> setA = {1, 2, 3, 4, 5};
+  Set<int> setB = {4, 5, 6, 7, 8};
+
+  print(" Elements of Set A: ");
+  for (int element in setA) {
+    print(element);
+  }
+
+  print("Elements of Set B:");
+  for (int element in setB) {
+    print(element);
+  }
+}
